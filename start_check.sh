285c92d7e7a424991265e39d82369732d0e9b548
lorenzodamora <damora.lorenzo.studente@itispaleocapa.it>
2024-02-10 15:49:16 +0100

    rename check_output

--- start_check.sh
+++ start_check.sh
@@ -2,5 +2,5 @@
 project_path="/home/ubuntu/Magnus/PycharmProj/persone/Adora/meteobot"
 cd "${project_path}"
 source "${project_path}/.python_venv/bin/activate"
-python -u check_output.py &
+python -u check_output_4.py &
 deactivate
